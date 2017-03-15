24d0c684aa17401848fa8856b0f66e995adf6852
rstar2 <cadb12new>
2017-03-15 21:25:32 +0200

    asd

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,9 +1,9 @@
 // Load the implementations that should be tested
-import { AppComponent } from './app.component';
+import { App } from './app';
 import { AppState } from './app.service';
 
 describe(`App`, () => {
-  let comp: AppComponent;
+  let comp: App = new App();
 
   it(`should be readly initialized`, () => {
     expect(comp).toBeDefined();
